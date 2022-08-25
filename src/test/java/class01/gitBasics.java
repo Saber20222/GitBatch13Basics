a70a2c0f12433c7cd17e5fc98037bdcb6977eafa
Saber20222 <sabersamim.ss@gmail.com>
2022-08-24 17:03:49 -0700

    temperturecheck

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,10 @@
+package class01;
+
+public class gitBasics {
+    public static void main(String[] args) {
+        // write down a code that checks the temperature from a website and then
+        // prints out that is it beatiful day or not
+
+        System.out.println(" Today is beautiful day");
+    }
+}
