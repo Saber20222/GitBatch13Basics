6e473b26f84be7ba1dee2810536921693c56a629
Saber20222 <sabersamim.ss@gmail.com>
2022-08-25 12:21:39 -0700

    added git ignore file

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -2,9 +2,9 @@ package class01;
 
 public class gitBasics {
     public static void main(String[] args) {
-        // write down a code that checks the temperature from a website and then
-        // prints out that is it beatiful day or not
-
-        System.out.println(" Today is beautiful day");
+//        write down a code that checks the temperature from a website and then
+//        prints out that is it a beautiful day or not
+        System.out.println("today is a not beautiful day");
     }
 }
+
